a801fddeb285a9297fdbfe1b24f11ff750ca7213
shine1594 <panamera1594@gmail.com>
2018-07-28 18:35:27 +0900

    모듈패턴 적용

--- todo.js
+++ todo.js
@@ -51,137 +51,139 @@ var findById = function (list, id) {
 // browser: window
 // node: global
 
-var todoApp = {
-  userName: '서한샘',
-  todos: [],
+var todoApp = (function () {
+  var userName = '서한샘';
+  var todos = [];
 
-  create: function (arg) {
-    var push = function (todo) {
-      this.todos.push(new Todo(todo));
-    }.bind(this);
+  return {
+    create: function (arg) {
+      var push = function (todo) {
+        todos.push(new Todo(todo));
+      };
 
-    // 인자 없거나
-    if (arg === undefined) {
-      push({
-        author: this.userName
-      });
-    }
+      // 인자 없거나
+      if (arg === undefined) {
+        push({
+          author: userName
+        });
+      }
 
-    // 배열
-    if (arg instanceof Array) {
-      arg.forEach(push);
-    } else if (arg instanceof Object) {
-      push(arg);
-    }
+      // 배열
+      if (arg instanceof Array) {
+        arg.forEach(push);
+      } else if (arg instanceof Object) {
+        push(arg);
+      }
 
-    // 조건 1: 함수에 인자를 전달하지 않으면 title과 author가 비어있는 todo 생성 (status: 'todo')
-    // var todo = todoApp.create();
-    // console.log(todo) => {id: 6, title: null, author: null, status: 'todo'};
+      // 조건 1: 함수에 인자를 전달하지 않으면 title과 author가 비어있는 todo 생성 (status: 'todo')
+      // var todo = todoApp.create();
+      // console.log(todo) => {id: 6, title: null, author: null, status: 'todo'};
 
-    // 조건 2: 함수에 todo 객체의 배열이 인자로 전달되면 현재 todos배열 뒤에 순차적으로 삽입
-  },
+      // 조건 2: 함수에 todo 객체의 배열이 인자로 전달되면 현재 todos배열 뒤에 순차적으로 삽입
+    },
 
-  read: function (arg) {
-    // todos 배열에서 id에 해당하는 할 일 객체 찾아서 반환
-    if (arg == undefined) return this.todos;
+    read: function (arg) {
+      // todos 배열에서 id에 해당하는 할 일 객체 찾아서 반환
+      if (arg == undefined) return todos;
 
-    var findTodo = id => findById(this.todos, id);
+      var findTodo = id => findById(todos, id);
 
-    if (isNumber(arg)) {
-      return findTodo(arg);
-    } else if (arg instanceof Array) {
-      return arg.map(findTodo).filter(isNotUndefined);
-    }
-
-
-    // arg = [null, 2, "3", 4];
-    // res = [undefined, {id:2}, undefined, {id:4}]
-    // [{id: 2}, {id: 4}]
+      if (isNumber(arg)) {
+        return findTodo(arg);
+      } else if (arg instanceof Array) {
+        return arg.map(findTodo).filter(isNotUndefined);
+      }
 
 
-    // 조건 1: 함수의 첫 번째 인자가 숫자 일 때 id로 간주하고 id에 해당하는 객체 반환
-    // 조건 2: 인자가 없으면 모든 할일 객체 배열로 반환
-    // 조건 3: 함수 인자로 id의 배열을 입력받으면 id에 해당하는 객체 배열 반환
-    //var todos2 = todos.find(i == id)
-    //return this.todos.title;
-    // 시간남으면
-    // 인자가 1개일때 id가 숫자 혹은 숫자의 배열이 아닌 경우 undefined 반환
-  },
+      // arg = [null, 2, "3", 4];
+      // res = [undefined, {id:2}, undefined, {id:4}]
+      // [{id: 2}, {id: 4}]
+
+
+      // 조건 1: 함수의 첫 번째 인자가 숫자 일 때 id로 간주하고 id에 해당하는 객체 반환
+      // 조건 2: 인자가 없으면 모든 할일 객체 배열로 반환
+      // 조건 3: 함수 인자로 id의 배열을 입력받으면 id에 해당하는 객체 배열 반환
+      //var todos2 = todos.find(i == id)
+      //return this.todos.title;
+      // 시간남으면
+      // 인자가 1개일때 id가 숫자 혹은 숫자의 배열이 아닌 경우 undefined 반환
+    },
 
 
 
-  update: function (arg) {
-    // todos 배열에 존재하는 todo 객체의 정보 수정
+    update: function (arg) {
+      // todos 배열에 존재하는 todo 객체의 정보 수정
+
+      /*
+       *  사용 예시
+       *  todoApp.update({id: 1, author: '서한샘', memo: 'javascript'});
+       */
+
+      const merge = props => {
+        const todo = findById(todos, props.id);
+
+        if (todo instanceof Object) {
+          Object.assign(todo, props);
+          console.log(todo.id);
+        }
+      }
+
+      if (arg instanceof Array) {
+        arg.forEach(merge);
+      } else if (arg instanceof Object) {
+        merge(arg);
+      }
+    },
+
+    // 조건 1. 객체 배열을 함수 인자로 받은 경우 해당하는 객체 정보에 알맞게 수정
 
     /*
      *  사용 예시
-     *  todoApp.update({id: 1, author: '서한샘', memo: 'javascript'});
+     *  todoApp.update(
+     *    [
+     *      {id: 1, author: '서한샘', memo: 'javascript'},
+     *      {id: 2, author: '황유성', memo: 'python'}
+     *    ]
+     *  );
      */
 
-    const merge = props => {
-      const todo = findById(this.todos, props.id);
+    // 조건 2. 변경 된 객체 id console.log로 출력 (여러개면 배열로)
 
-      if (todo instanceof Object) {
-        Object.assign(todo, props);
-        console.log(todo.id);
+    // 조건 3. id가 없는 객체거나 해당하는 id가 없으면 무시
+
+
+
+    delete: function (arg) {
+      // id를 입력받아 todos배열에서 해당하는 객체를 삭제
+
+      // var str = "삭제할 id를 입력해주세요";
+      // var content =Number(prompt(str, ''));
+      // const idx = todos.indexOf(content);
+      // todos.splice(idx, idx+1);
+
+      const removeAt = index => todos.splice(index, 1);
+
+      const removeById = id => {
+        const index = todos.findIndex(item => item.id === id);
+
+        if (index !== -1) {
+          removeAt(index);
+          console.log(id);
+        }
       }
-    }
 
-    if (arg instanceof Array) {
-      arg.forEach(merge);
-    } else if (arg instanceof Object) {
-      merge(arg);
-    }
-  },
-
-  // 조건 1. 객체 배열을 함수 인자로 받은 경우 해당하는 객체 정보에 알맞게 수정
-
-  /*
-   *  사용 예시
-   *  todoApp.update(
-   *    [
-   *      {id: 1, author: '서한샘', memo: 'javascript'},
-   *      {id: 2, author: '황유성', memo: 'python'}
-   *    ]
-   *  );
-   */
-
-  // 조건 2. 변경 된 객체 id console.log로 출력 (여러개면 배열로)
-
-  // 조건 3. id가 없는 객체거나 해당하는 id가 없으면 무시
-
-
-
-  delete: function (arg) {
-    // id를 입력받아 todos배열에서 해당하는 객체를 삭제
-
-    // var str = "삭제할 id를 입력해주세요";
-    // var content =Number(prompt(str, ''));
-    // const idx = this.todos.indexOf(content);
-    // this.todos.splice(idx, idx+1);
-
-    const removeAt = index => this.todos.splice(index, 1);
-
-    const removeById = id => {
-      const index = this.todos.findIndex(item => item.id === id);
-
-      if (index !== -1) {
-        removeAt(index);
-        console.log(id);
+      if (isNumber(arg)) {
+        removeById(arg);
+      } else if (arg instanceof Array) {
+        arg.forEach(removeById);
       }
-    }
 
-    if (isNumber(arg)) {
-      removeById(arg);
-    } else if (arg instanceof Array) {
-      arg.forEach(removeById);
+      // 조건 1. 객체 배열을 함수 인자로 받은 경우 해당하는 객체 모두 삭제
+      // 조건 2. 삭제 된 객체 id console.log로 출력 (여러개면 배열로)
+      // 조건 3. 없는 id인 경우 무시
     }
-
-    // 조건 1. 객체 배열을 함수 인자로 받은 경우 해당하는 객체 모두 삭제
-    // 조건 2. 삭제 된 객체 id console.log로 출력 (여러개면 배열로)
-    // 조건 3. 없는 id인 경우 무시
   }
-};
+})();
 
 
 
